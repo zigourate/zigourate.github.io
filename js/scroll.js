50fdfaf803014e95d10a3190469b9088af50bd53
zigourate <142616733+zigourate@users.noreply.github.com>
2024-01-22 11:35:04 -0500

    Delete js directory

--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-$(document).on('click','a[href^="#"]',function(e){
-	var id=$(this).attr('href');
-	var $id=$(id);
-	if($id.length===0){
-		return;
-}
-e.preventDefault();
-var pos=$id.offset().top;
-$('body,html').animate({scrollTop:pos},1000);});

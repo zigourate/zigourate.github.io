3b3ab59549c4cb5b71ae2dcef9c6c9a319aa9436
zigourate <142616733+zigourate@users.noreply.github.com>
2024-01-22 11:42:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        { pageLanguage: 'en', includedLanguages: 'en,fr' },
+        'google_translate_element'
+    );
+}
+
